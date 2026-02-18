247e6c2eaa7086a450e41c0fbd4e3251fb2db952
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2026-02-18 22:41:49 +0000

    ✨	feat : Security 설정

---
+++ PromptLAB/src/main/java/org/example/promptlab/global/util/RedisUtil.java
@@ -0,0 +1,52 @@
+package org.example.promptlab.global.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final StringRedisTemplate redisBlackListTemplate;
+
+    public void save(String key, Object val, Long time, TimeUnit timeUnit) {
+        redisTemplate.opsForValue().set(key, val, time, timeUnit);
+    }
+
+    public boolean hasKey(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public boolean delete(String key) {
+        return Boolean.TRUE.equals(redisTemplate.delete(key));
+    }
+
+
+    // BlackList
+    public void setBlackList(String token, long expirationMillis) {
+        redisBlackListTemplate.opsForValue()
+                .set(token, "logout", expirationMillis, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean isBlackListed(String token) {
+        return Boolean.TRUE.equals(redisBlackListTemplate.hasKey(token));
+    }
+
+    public boolean deleteBlackList(String key) {
+        return redisBlackListTemplate.delete(key);
+    }
+
+    public boolean hasKeyBlackList(String key) {
+        return redisBlackListTemplate.hasKey(key);
+    }
+
+}
