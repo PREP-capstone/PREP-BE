1988a681c2cc6e4a54a74842d04a8230c2c4f309
MunChaerin <164905124+MunChaerin@users.noreply.github.com>
2026-02-19 15:39:24 +0000

    ♻️	refactor : 주소 수정

--- PromptLAB/src/main/java/org/example/promptlab/global/config/CorsConfig.java
+++ PromptLAB/src/main/java/org/example/promptlab/global/config/CorsConfig.java
@@ -24,7 +24,7 @@ public class CorsConfig implements WebMvcConfigurer {
         allowedOriginPatterns.add("http://localhost:5501");
         allowedOriginPatterns.add("http://localhost:3453");
         allowedOriginPatterns.add("http://localhost:8800");
-        allowedOriginPatterns.add("http://prompt-lab");
+        allowedOriginPatterns.add("http://prompt-lab.shop");
 
         corsConfiguration.setAllowedOrigins(allowedOriginPatterns);
 
